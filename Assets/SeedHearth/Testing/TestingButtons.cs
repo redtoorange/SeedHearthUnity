8617542fd728208e87e57d25bb0b07af29ec6361
redtoorange <redtoorange@gmail.com>
2023-05-12 08:09:59 -0400

    Moved code into assembly definitions

--- Assets/SeedHearth/Testing/TestingButtons.cs
+++ Assets/SeedHearth/Testing/TestingButtons.cs
@@ -9,12 +9,12 @@ namespace SeedHearth.Testing
         [SerializeField] private int handSize = 4;
         [SerializeField] private int staminaAmount = 4;
         
-        [SerializeField] private ResourceClicker staminaClicker;
+        // [SerializeField] private ResourceClicker staminaClicker;
         [SerializeField] private CardController cardController;
         
         public void DrawNewHand()
         {
-            staminaClicker.SetAmount(staminaAmount);
+            // staminaClicker.SetAmount(staminaAmount);
             cardController.DiscardHand();
 
             for (int i = 0; i < handSize; i++)
