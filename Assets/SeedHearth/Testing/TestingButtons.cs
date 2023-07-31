03c98228358d2b4c47e56330f8e12de55611d009
Andrew McGuiness <redtoorange@gmail.com>
2023-07-31 07:30:26 -0400

    Updated to new version of Unity

--- Assets/SeedHearth/Testing/TestingButtons.cs
+++ Assets/SeedHearth/Testing/TestingButtons.cs
@@ -1,5 +1,4 @@
 ﻿using SeedHearth.Cards;
-using SeedHearth.ResourceDisplay;
 using UnityEngine;
 
 namespace SeedHearth.Testing
@@ -8,13 +7,11 @@ namespace SeedHearth.Testing
     {
         [SerializeField] private int handSize = 4;
         [SerializeField] private int staminaAmount = 4;
-        
-        // [SerializeField] private ResourceClicker staminaClicker;
+
         [SerializeField] private CardController cardController;
-        
+
         public void DrawNewHand()
         {
-            // staminaClicker.SetAmount(staminaAmount);
             cardController.DiscardHand();
 
             for (int i = 0; i < handSize; i++)
