f5a1c64728168721e318a2fc4f9ab236d068e282
Andrew McGuiness <redtoorange@gmail.com>
2023-09-07 06:17:00 -0400

    Converted card tweens to DOTween

--- Assets/SeedHearth/Cards/Controllers/CardMovementController.cs
+++ Assets/SeedHearth/Cards/Controllers/CardMovementController.cs
@@ -1,4 +1,6 @@
 ﻿using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using SeedHearth.Data;
 using UnityEngine;
 
@@ -8,6 +10,7 @@ namespace SeedHearth.Cards.Controllers
     {
         [SerializeField] private float movementTime = 10.0f;
         private RectTransform rectTransform;
+        private TweenerCore<Vector3, Vector3, VectorOptions> moveTween;
 
         public override void Initialize(Card pCard, CardData pCardData)
         {
@@ -18,15 +21,22 @@ namespace SeedHearth.Cards.Controllers
 
         public void MoveTo(Vector2 newPosition)
         {
-            rectTransform.DOKill();
-            rectTransform.DOMove(newPosition, movementTime)
+            if (moveTween != null)
+            {
+                moveTween.Kill();
+            }
+
+            moveTween = rectTransform.DOMove(newPosition, movementTime)
                 // .SetSpeedBased(true)
                 .SetEase(Ease.Linear);
         }
 
         public void CancelMovement()
         {
-            rectTransform.DOKill();
+            if (moveTween != null)
+            {
+                moveTween.Kill();
+            }
         }
     }
 }
